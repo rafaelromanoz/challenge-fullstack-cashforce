58e08a0b57491acafa13311c08e078be57621e63
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2022-12-26 11:42:10 -0300

    chore: fix configuration eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,13 @@
-import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const PORT = 5000;
+const PORT = 5000
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
